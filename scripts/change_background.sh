593935bf94add0b8ddae571519c74329a4adea56
Zakhar Starinski <zakharstarinski17@gmail.com>
2024-11-14 22:50:42 +0000

    changed background engine

--- scripts/change_background.sh
+++ scripts/change_background.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
 # finds a random image in ~/wallpapers and sets it as background
-swaymsg output eDP-1 bg $(find ~/wallpapers -type f | grep -v .git | shuf -n 1) fill #111111
+LOCCATION=$(find ~/wallpapers -type f | grep -v .git | shuf -n 1)
+echo $LOCCATION > $HOME/cur_background_image
+swww img $LOCCATION --transition-step 30 --transition-duration 0.8 --transition-fps 60 -t wipe
+notify-send "changed to $LOCCATION" --icon=$LOCCATION
+
+# command for swaybg
+# swaymsg output HDMI-A-1 bg $LOCCATION fill '#000000'
